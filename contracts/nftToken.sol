6a578c8c2a45166785da60fd1c2d052007ac4095
Michelle David <rachaelholy@gmail.com>
2022-04-21 12:18:40 +0100

    Imported openzeppelin

--- contracts/nftToken.sol
+++ contracts/nftToken.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
+//@notice declare the solidity compiler 
 pragma solidity ^0.8.1;
+
+// Import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MichyNft {
-    constructor() {
+contract MichyNft is ERC721URIStorage  {
+   // @notice helps to keep tract of TokensIds 
+    using counters for counters.counter; 
+    counters.counter private _tokensId; 
+
+    constructor() ERC721 ("MichyToken", "MTK") {
         console.log("Hey, this is Michy's nft");
     }
+
+    
 }
