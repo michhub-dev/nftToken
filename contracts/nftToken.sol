8d66360805ee33bb3dfa91a27d401668b5ffc679
Michelle David <rachaelholy@gmail.com>
2022-04-22 15:09:26 +0100

    Updated to base64 encoded svg

--- contracts/nftToken.sol
+++ contracts/nftToken.sol
@@ -26,7 +26,7 @@ contract MichyNft is ERC721URIStorage  {
         
         
         //@notice set the nft data
-        _setTokenURI(newNft, "https://jsonkeeper.com/b/NPDP");
+        _setTokenURI(newNft, "data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LGRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaTgrQ2lBZ1BIUmxlSFFnZUQwaU5UQWxJaUI1UFNJMU1DVWlJR1J2YldsdVlXNTBMV0poYzJWc2FXNWxQU0p0YVdSa2JHVWlJSFJsZUhRdFlXNWphRzl5UFNKdGFXUmtiR1VpSUhOMGVXeGxQU0ptYVd4c09pTm1abVk3Wm05dWRDMW1ZVzFwYkhrNmMyVnlhV1k3Wm05dWRDMXphWHBsT2pFMGNIZ2lQazFsYldWTWIzSmtQQzkwWlhoMFBnbzhMM04yWno0PSIKfQ==");
         console.log("NFT has been minted to %s..", msg.sender);
 
         //@notice increment the counter for the next minted nft 
